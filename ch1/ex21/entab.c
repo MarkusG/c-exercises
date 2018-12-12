d48a49a52e8500509d26f0151ecf91bb3e363359
Mark Gross <mark.gross2001@gmail.com>
2018-12-12 14:35:56 -0800

    Works except for when we need tabs < TABSTOP

--- ch1/ex21/entab.c
+++ ch1/ex21/entab.c
@@ -27,24 +27,17 @@ int main(void)
 
 		for (int i = 0; (c = line[i]) != '\0'; i++)
 		{
-			if (c == ' ' && i > 0) // don't do this if we're at 0, because i % TABSTOP will be 0
+			if (c == ' ')
 			{
-				int tabs = 0;
-				int numSpaces = 0;
 				int remainingSpaces;
 				// every time we pass a tab stop, add a tab
-				// make sure to not mess up i here
 				while (line[i] == ' ')
 				{
-					numSpaces++;
 					if ((i + 1) % TABSTOP == 0)
-						tabs++;
+						putchar('\t');
 					i++;
 				}
 				i--;
-
-				for (int j = 0; j < tabs; j++)
-					putchar('\t');
 
 				if ((i + 1) % TABSTOP != 0)
 				{
